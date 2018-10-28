1e049c205bd63e483f8244753738959d08b1b035
Patrick Seafield <seafielp@oregonstate.edu>
2018-10-28 16:01:31 -0700

    Added max connection limit necessary for flip servers.

--- chatserve/chatserve.py
+++ chatserve/chatserve.py
@@ -30,7 +30,7 @@ if __name__ == '__main__':
     # Set up the socket and listen for the chatclient.
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((host, port))
-        s.listen()
+        s.listen(5)
         keepListening = True
         while keepListening:
             conn, addr = s.accept()
